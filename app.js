a51bbc005b9ca0867b4f807686513f1beae5bdac
ken597 <3210315723@qq.com>
2024-07-10 09:34:31 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+// app.js
+App({
+  onLaunch() {
+  },
+  globalData: {
+    userInfo: null,
+    //IAM的网址
+    iamEndpoint: "iam.cn-north-4.myhuaweicloud.com",   
+    // 账号名
+    userName: "GT-m0_64954999",
+    // IAM用户名
+    iamUserName: "ken",
+    // IAM密码
+    iamUserPassword: "Wang152031009.",
+    // 所属项目名
+    productName: "cn-north-4",
+    // IoTDA的网址
+    iotDAEndpoint: "iotda.cn-north-4.myhuaweicloud.com",
+    // 产品ID
+    project_id: "6669a7e36bc31504f06e7a2f",
+    // 项目ID
+    product_id: "	eb331105c1464de8ac0c3598aac6241e",
+    // 设备ID
+    device_id: "6669a7e36bc31504f06e7a2f_hi3861_farmm",
+
+    color: 'rgb(0,0,0)'
+  }
+})
